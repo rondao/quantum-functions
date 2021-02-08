bdbe4b3e8c248d60206c4378cf644a6c1ecd7ac7
Rafael Rondao <rafael.rondao@gmail.com>
2021-02-08 17:36:50 -0300

    Add Firebase authentication
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,40 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import firebase from "firebase";
 
 import * as express from "express";
 
-import { Class } from "../src/model/types";
+import { Class, Signup } from "../src/model/types";
 
 admin.initializeApp();
+firebase.initializeApp({
+  apiKey: "AIzaSyDSkARToMknYJSbmO_E-rYbIwOCVyqef3o",
+  authDomain: "quantum-c5194.firebaseapp.com",
+  databaseURL: "https://quantum-c5194-default-rtdb.firebaseio.com",
+  projectId: "quantum-c5194",
+  storageBucket: "quantum-c5194.appspot.com",
+  messagingSenderId: "506943089189",
+  appId: "1:506943089189:web:d2ff41135fa7d6a5526dcc",
+  measurementId: "G-5TEV0K2FSD",
+});
+
 const app = express();
+
+app.post("/signup", (request, response) => {
+  const newUser = request.body as Signup;
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then((data) => {
+      return response
+        .status(201)
+        .json({ message: `User ${data.user?.uid} signed up successfully` });
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: err.code });
+    });
+});
 
 app.get("/classes", (request, response) => {
   admin
@@ -38,11 +66,11 @@ app.post("/class", (request, response) => {
     .collection("classes")
     .add(newClass)
     .then((doc) => {
-      response.json({ message: `Class ${doc.id} created successfully` });
+      return response.json({ message: `Class ${doc.id} created successfully` });
     })
     .catch((err) => {
       console.error(err);
-      response.status(500).json({ error: "Something went wrong." });
+      return response.status(500).json({ error: "Something went wrong." });
     });
 });
 
--- functions/src/model/types.ts
+++ functions/src/model/types.ts
@@ -1,3 +1,10 @@
+export interface Signup {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  name: string;
+}
+
 export interface Class {
   id?: string;
   name: string;
