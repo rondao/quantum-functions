994ee5302627d42898465c6c1d71e56d9c7a243b
Rafael Rondao <rafael.rondao@gmail.com>
2021-02-08 10:30:23 -0300

    Add endpoint to get all Classes
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,26 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+import { Class } from "../src/model/types";
+
+admin.initializeApp();
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
+});
+
+export const getClasses = functions.https.onRequest((request, response) => {
+  admin
+    .firestore()
+    .collection("classes")
+    .get()
+    .then((data) => {
+      let classes = <Class[]>[];
+      data.forEach((doc) => {
+        classes.push(doc.data() as Class);
+      });
+      return response.json(classes);
+    })
+    .catch((err) => console.error(err));
 });
---
+++ functions/src/model/types.ts
@@ -0,0 +1,5 @@
+export interface Class {
+  name: string;
+  subject: string;
+  professor: string;
+}
