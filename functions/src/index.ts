521d9cd7ced23729bc6ad1e446ea026b5fa3aab6
Rafael Rondao <rafael.rondao@gmail.com>
2021-02-08 12:09:03 -0300

    Import Express for endpoints management
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,14 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
+import * as express from "express";
+
 import { Class } from "../src/model/types";
 
 admin.initializeApp();
+const app = express();
 
-export const getClasses = functions.https.onRequest((request, response) => {
+app.get("/classes", (request, response) => {
   admin
     .firestore()
     .collection("classes")
@@ -20,30 +23,24 @@ export const getClasses = functions.https.onRequest((request, response) => {
     .catch((err) => console.error(err));
 });
 
-export const createClasses = functions.https.onRequest((request, response) => {
-  switch (request.method) {
-    case "POST":
-      const newClass: Class = {
-        name: request.body.name,
-        subject: request.body.subject,
-        professor: request.body.professor,
-      };
+app.post("/class", (request, response) => {
+  const newClass: Class = {
+    name: request.body.name,
+    subject: request.body.subject,
+    professor: request.body.professor,
+  };
 
-      admin
-        .firestore()
-        .collection("classes")
-        .add(newClass)
-        .then((doc) => {
-          response.json({ message: `Class ${doc.id} created successfully` });
-        })
-        .catch((err) => {
-          console.error(err);
-          response.status(500).json({ error: "Something went wrong." });
-        });
-      break;
-
-    default:
-      response.status(400).json({ error: "Method not allowed. " });
-      break;
-  }
+  admin
+    .firestore()
+    .collection("classes")
+    .add(newClass)
+    .then((doc) => {
+      response.json({ message: `Class ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      console.error(err);
+      response.status(500).json({ error: "Something went wrong." });
+    });
 });
+
+export const api = functions.https.onRequest(app);
