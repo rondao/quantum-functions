9f694e41f8633e39fdde4908ad95204397c135f0
Rafael Rondao <rafael.rondao@gmail.com>
2021-03-05 14:21:57 -0300

    Test /classes when empty
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- functions/src/index.test.ts
+++ functions/src/index.test.ts
@@ -10,46 +10,58 @@ import { Class } from "../src/model/types";
 const database = admin.firestore();
 
 describe("Test /classes endpoint.", () => {
-  const testData: Class[] = [
-    {
-      name: "Class 1",
-      subject: "Subject 1",
-      professor: "Professor 1",
-    },
-    {
-      name: "Class 2",
-      subject: "Subject 2",
-      professor: "Professor 2",
-    },
-    {
-      name: "Class 2",
-      subject: "Subject 3",
-      professor: "Professor 1",
-    },
-  ];
-
-  beforeAll(async (done) => {
-    for (const doc of testData) {
-      await database.collection("classes").add(doc);
-    }
-    done();
-  });
-
-  test("Get all registered classes", async () => {
+  test("Get all classes when empty.", async (done) => {
     const res = await request(app).get("/classes").expect(200);
-    expect(res.body).toHaveLength(testData.length);
-    expect(res.body).toEqual(
-      expect.arrayContaining(
-        testData.map((data) => expect.objectContaining(data))
-      )
-    );
+
+    expect(res.body).toHaveLength(0);
+    expect(res.body).toEqual([]);
+    done();
   });
 
-  afterAll(async (done) => {
-    const documents = await database.collection("classes").get();
-    for (const doc of documents.docs) {
-      await database.collection("classes").doc(doc.id).delete();
-    }
-    done();
+  describe("Test with pre-registered classes.", () => {
+    const testData: Class[] = [
+      {
+        name: "Class 1",
+        subject: "Subject 1",
+        professor: "Professor 1",
+      },
+      {
+        name: "Class 2",
+        subject: "Subject 2",
+        professor: "Professor 2",
+      },
+      {
+        name: "Class 2",
+        subject: "Subject 3",
+        professor: "Professor 1",
+      },
+    ];
+
+    beforeAll(async (done) => {
+      for (const doc of testData) {
+        await database.collection("classes").add(doc);
+      }
+      done();
+    });
+
+    test("Get all classes.", async (done) => {
+      const res = await request(app).get("/classes").expect(200);
+
+      expect(res.body).toHaveLength(testData.length);
+      expect(res.body).toEqual(
+        expect.arrayContaining(
+          testData.map((data) => expect.objectContaining(data))
+        )
+      );
+      done();
+    });
+
+    afterEach(async (done) => {
+      const documents = await database.collection("classes").get();
+      for (const doc of documents.docs) {
+        await database.collection("classes").doc(doc.id).delete();
+      }
+      done();
+    });
   });
 });
